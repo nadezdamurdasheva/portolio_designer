1c2c4a66e2f50149aeab012ca7f5e12a13139f46
nadezdamurdasheva <madezdamurdasheva@yandex.ru>
2023-04-19 12:00:59 +0300

    added styles for burger and started i18n

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './Menu.css';
 import { MenuItem } from "./MenuItem/MenuItem";
 import burger from "../../images/burger.svg";
@@ -6,14 +6,18 @@ import burger from "../../images/burger.svg";
 const menuArray = ['Home','About me','Skills','Portfolio','Contacts'];
 
 export const Menu = () => {
+  const [menuActive,setMenuActive] = useState(false);
+
   return (
   <nav className="links_container">
-    <ul className="links">
-      {menuArray.map(el => {
-        return (<MenuItem key={el} nameLink={el}/>)
-      })}
-    </ul>
-    <img className="burger" alt="menu" src={burger}/>
+    <div className="links_img_container">
+      <ul className={menuActive ? "links" : "links links-hide"}>
+        {menuArray.map(el => {
+          return (<MenuItem key={el} nameLink={el}/>)
+        })}
+      </ul>
+      <img className="burger" alt="menu" src={burger} onClick={() => setMenuActive(!menuActive)}/>
+    </div>
   </nav>
   );
 };
